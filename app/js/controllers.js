5368c9c5c7cd8dd62090d82240e6b0145ea8d70b
Aiden Yang <ayang@Aidens-MacBook-Pro.local>
2014-01-03 00:17:02 -0500

    Initial commit of alyang.io

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,12 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('HomeCtrl', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('ResumeCtrl', [function() {
+
+  }])
+  .controller('PortfolioCtrl', [function() {
 
   }]);
